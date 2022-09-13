bbc251808a2979784a22ed117deddb766ea7c8e5
COHL7500 <cohl7500@gmail.com>
2022-09-13 13:23:00 +0200

    Implement Filter

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -6,7 +6,7 @@ using Assignment1;
 public class IteratorsTests
 {
     [Fact]
-    static void Flatten_should_return_a_stream_of_T()
+    static void Flatten__returns_a_stream_of_T()
     {
         // Arrange
         int[] array123 = {1, 2, 3};
@@ -21,4 +21,22 @@ public class IteratorsTests
         spaghetti.Should()
             .BeEquivalentTo(expected.AsEnumerable());
     }
+
+    [Fact]
+    static void Filter_returns_true_true()
+    {
+        // Arrange
+        int[] array1324 = {1, 3, 2, 4};
+        Predicate<int> even = Even;
+        bool Even(int i) => i % 2 == 0;
+        int[] expected = {2, 4};
+
+        // Act
+        var lasagne = Iterators.Filter(array1324.AsEnumerable(), even);
+        
+        // Assert
+        Assert.Equal(expected, lasagne);
+
+    }
+
 }
